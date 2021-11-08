b852681264e7839cc05eb254a03c267bb2749bba
SorinTautu1991 <sorin.tautu@gmail.com>
2021-11-08 11:18:07 +0200

    Drawing a line geometry is deprecated and was removed from api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useMemo } from "react";
 import "./App.css";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, extend } from "@react-three/fiber";
 import Box from "./components/Box";
 import Orbit from "./components/Orbit";
 
 const App = () => {
+  const vertices = useMemo(() => {
+    let p = [
+      [0, 0, 0],
+      [0, 1, 1],
+      [0, 1, -1],
+    ];
+    const flatArr = p.reduce((ac, cv) => [...cv, ...ac], []);
+    return new Float32Array(flatArr);
+  }, []);
+
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas style={{ background: "black" }} camera={{ position: [3, 3, 3] }}>
-        <Box position={[-1, 1, 0]} />
-        <axesHelper args={[5]} />
         <Orbit />
+        <axesHelper args={[5]} />
+        <Box position={[-1, 1, 0]} />
+        <line>
+          <bufferGeometry attach="geometry">
+            <bufferAttribute
+              attachObject={["attributes", "position"]}
+              array={vertices}
+              itemSize={3}
+              count={vertices.length / 3}
+            />
+          </bufferGeometry>
+          <lineBasicMaterial color="red" />
+        </line>
       </Canvas>
     </div>
   );
