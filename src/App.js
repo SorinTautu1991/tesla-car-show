8c604692b490f1cc0e0541facbc0ab02065778b9
SorinTautu1991 <sorin.tautu@gmail.com>
2021-11-08 10:05:28 +0200

    Setting up a scene using plain old Three.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { render } from "@testing-library/react";
 import React from "react";
+import * as THREE from "three";
 
 const App = () => {
-  return <div>App</div>;
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  const renderer = new THREE.WebGL1Renderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.innerHTML = "";
+  document.body.appendChild(renderer.domElement);
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshBasicMaterial({ color: "blue" });
+  camera.position.z = 5;
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+  };
+  animate();
+  window.addEventListener("resize", () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  });
+  return null;
 };
 
 export default App;
